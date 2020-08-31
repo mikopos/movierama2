dce205be3f2e016a194bb0c332589a92331bf2b0
Marios <mikopos@gmail.com>
2020-08-31 17:05:56 +0300

    first commit

---
+++ src/main/java/com/marios/gavriil/movierama2/Movierama2Application.java
@@ -0,0 +1,13 @@
+package com.marios.gavriil.movierama2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movierama2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Movierama2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/marios/gavriil/movierama2/Movierama2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marios.gavriil.movierama2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Movierama2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
